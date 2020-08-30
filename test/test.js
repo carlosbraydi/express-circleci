cfe265c292bbf72da2fdef14093531f754557b33
cazb999 <cazb999@hotmail.com>
2020-08-30 16:41:33 -0500

    nueva prueba

--- test/test.js
+++ test/test.js
@@ -1,1 +1,15 @@
-console.log('ok');
+const chai = require('chai');
+const assert = chai.assert;
+const should = chai.should();
+const expect = chai.expect;
+const services = require('../services/movies');
+
+describe('Testing assert function: ', () => {
+    describe('Check getMovies Function', () => {
+        it('Check the returned value using : assert.equal(value, values): ', async () => {
+            const moviesServices = new services();
+            let movies = await moviesServices.getMovies();
+            assert.equal(typeof(movies), typeof({}));
+        });
+    });
+});
